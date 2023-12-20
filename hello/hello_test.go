44260bb41a511ac22e8e8af1a83cdfb94bf4ed59
richarda23 <richard.c.adams@infinityworks.com>
2023-12-20 13:41:20 +0000

    iteration

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,34 +1,30 @@
 package main
 
-import "testing"
+import (
+	"hello/testhelper"
+	"testing"
+)
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Brian", "")
 		wanted := "Hello, Brian"
-		AssertCorrectMessage(t, got, wanted)
+		testhelper.AssertCorrectMessage(t, got, wanted)
 	})
 	t.Run("general hllo", func(t *testing.T) {
 		got := Hello("", "")
 		wanted := "Hello, World"
-		AssertCorrectMessage(t, got, wanted)
+		testhelper.AssertCorrectMessage(t, got, wanted)
 	})
 	t.Run("hello in spanish", func(t *testing.T) {
 		got := Hello("", "spanish")
 		wanted := "Hola, World"
-		AssertCorrectMessage(t, got, wanted)
+		testhelper.AssertCorrectMessage(t, got, wanted)
 	})
 
 	t.Run("hello in french", func(t *testing.T) {
 		got := Hello("emilie", "french")
 		wanted := "Bonjour, emilie"
-		AssertCorrectMessage(t, got, wanted)
+		testhelper.AssertCorrectMessage(t, got, wanted)
 	})
-}
-
-func AssertCorrectMessage(t testing.TB, got, wanted string) {
-	t.Helper()
-	if got != wanted {
-		t.Errorf("wanted %q but got %q", wanted, got)
-	}
 }
--- integers/adder_test.go
+++ integers/adder_test.go
@@ -2,6 +2,7 @@ package integers
 
 import (
 	"fmt"
+	"hello/testhelper"
 	"testing"
 )
 
@@ -18,12 +19,5 @@ func ExampleAdd() {
 func TestAdd(t *testing.T) {
 	sum := Add(2, 2)
 	wanted := 4
-	AssertInteger(t, wanted, sum)
-}
-
-func AssertInteger(t testing.TB, wanted, got int) {
-	t.Helper()
-	if wanted != got {
-		t.Errorf("Wanted %d but got %d", wanted, got)
-	}
+	testhelper.AssertInteger(t, wanted, sum)
 }
