2aa4f43f712ab6987c5609020974efd5313f4f28
richarda23 <richard.c.adams@infinityworks.com>
2023-12-20 11:22:50 +0000

    hello finished

--- hello.go
+++ hello.go
@@ -4,16 +4,26 @@ import "fmt"
 
 const englishPrefix = "Hello,"
 const spanishPrefix = "Hola,"
+const frenchPrefix = "Bonjour,"
 
 func Hello(name, lang string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishPrefix
-	if lang == "spanish" {
-		prefix = spanishPrefix
-	}
+	prefix := GreetingPrefix(lang)
+
 	return fmt.Sprintf("%s %s", prefix, name)
+}
+
+func GreetingPrefix(lang string) string {
+	prefix := englishPrefix
+	switch lang {
+	case "spanish":
+		prefix = spanishPrefix
+	case "french":
+		prefix = frenchPrefix
+	}
+	return prefix
 }
 func main() {
 	fmt.Println(Hello("Bob", ""))
--- hello_test.go
+++ hello_test.go
@@ -4,14 +4,24 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Brian")
+		got := Hello("Brian", "")
 		wanted := "Hello, Brian"
 		AssertCorrectMessage(t, got, wanted)
-
 	})
 	t.Run("general hllo", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		wanted := "Hello, World"
+		AssertCorrectMessage(t, got, wanted)
+	})
+	t.Run("hello in spanish", func(t *testing.T) {
+		got := Hello("", "spanish")
+		wanted := "Hola, World"
+		AssertCorrectMessage(t, got, wanted)
+	})
+
+	t.Run("hello in french", func(t *testing.T) {
+		got := Hello("emilie", "french")
+		wanted := "Bonjour, emilie"
 		AssertCorrectMessage(t, got, wanted)
 	})
 }
